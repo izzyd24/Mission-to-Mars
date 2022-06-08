a62c9c14c72ac8f767e1900c19c3ce7fb7429a3a
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-07 22:21:03 -0700

    10.5.2 complete

--- scraping.py
+++ scraping.py
@@ -1,3 +1,6 @@
+# update to integreate scraping code so Flask can use it
+# 10.5.2 asks to refactor the scrapping.py to include functions and add try/error 
+
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
@@ -8,60 +11,86 @@ from webdriver_manager.chrome import ChromeDriverManager
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the Mars news site
-url = 'https://redplanetscience.com/'
-browser.visit(url)
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+### NEWS AND TITLE PARA 
+# add the argument browser, to tell python to use browser var from above
+def mars_news(browser):
 
-# Convert the browser html to a soup object and then quit the browser
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    # Scrape Mars News
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
 
-slide_elem = news_soup.select_one('div.list_text')
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-slide_elem.find('div', class_='content_title')
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-# Use the parent element to find the first a tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+    # Add try/except for error handling
+    # most common error in scraping is when webpage format has changed 
+    # ... no longer matches our HTML called elements
+    try:
+        # tell python to look for these elements, and if error...
+        # then cont. to run the remainder of code
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    except AttributeError:
+    # if attribute error return nothing
+        return None, None
+    # do not print, but simply end the function by returning the newstitle and newsp
+    return news_title, news_p
+   
+  
+### JPL Space Images Featured Image
+def featured_image(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# ## JPL Space Images Featured Image
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    except AttributeError:
+        return None
 
-# find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
-# Use the base url to create an absolute url
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    return img_url
 
-# ## Mars Facts
+### Mars Facts
+def mars_facts():
+    # Add try/except for error handling
+    # baseexception = when built ins are encountered, won't handle user-defined exceptions
+    try:
+        # Use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.head()
+    except BaseException:
+        # removed the print statements, and return none if base exception
+        return None
 
-df.columns=['Description', 'Mars', 'Earth']
-df.set_index('Description', inplace=True)
-df
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
-df.to_html()
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
 
 browser.quit()
