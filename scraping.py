2da2434cb9074f83b99e3004f5aaa9e3956270af
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-07 22:32:30 -0700

    10.5 complete

--- scraping.py
+++ scraping.py
@@ -5,7 +5,37 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
+
+# addition from 10.5.3
+# function to initialize browser, create data dict, end webdriver
+# return scraped data
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    # tells us that news title, news para variables = marsnews function 
+    # lets us pull the data
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
+    # this dict does: 
+    # 1. runs all functions 
+    # 2. stores results
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # function is complete
+    # closes broswer/stopwebdriver
+    browser.quit()
+     # returns data dict. 
+    return data
 
 # Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -93,4 +123,12 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
-browser.quit()
+# final block tells flask that our script is complete
+# prints the results of scraping to terminal 
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
+
+# add 10.5.3 code--- update data stored in mongo each time its ran 
+# script to establish link between scraped data and our database via mongodb
