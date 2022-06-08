5bf84789dfb78615da12790d3aae4274a02347a7
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-07 22:05:51 -0700

    10.5.1 complete

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
+
 # set up flask 
 app = Flask(__name__)
 
@@ -16,9 +17,36 @@ mongo = PyMongo(app)
 
 # set up routes for flask 
 
-# 1st route for html page
+# ROUTE #1 for html page
+# tell Flask to display home page, index.html
 @app.route("/")
+# def function to accomplish...
 def index():
+   # find mars collection within database
+   # assign path to mars var to reference easily
    mars = mongo.db.mars.find_one()
+   # tell flask to return html template using our index.html file
+   # mars = mars tells python to use mars collection in mongodb
    return render_template("index.html", mars=mars)
 
+# ROUTE #2 for scraping process
+# defines the route Flask will use
+# scrape will run the function
+@app.route("/scrape")
+# define it
+def scrape():
+   # assign new var to point to mongo database from earlier 
+   mars = mongo.db.mars
+   # new var to hold newly scraped data on the scraping.py file
+   mars_data = scraping.scrape_all()
+   # gathered the data, so now we update the database with updateone function
+   # TYPICAL FORMAT: .update_one(query_param, {$set: data}, options)
+   # we add data if not recorded previously
+   # upser = true to tell mongo to create new doc if one doesn't exist
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   # once scraped, navigate back to page with updated content
+   return redirect('/', code=302)
+
+# tell flask to run
+if __name__ == "__main__":
+   app.run()
